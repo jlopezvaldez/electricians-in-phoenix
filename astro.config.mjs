9bad9e444f6b57bd6f2cad8b0f565937946a54f4
jonathan <jlopezv0324@gmail.com>
2024-04-04 15:56:50 -0700

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: netlify()
+  output: 'server',
+  adapter: netlify(),
 });
